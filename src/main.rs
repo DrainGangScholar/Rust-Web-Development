c79289f2e46245af1c22e0095bbf57e7677e0eb3
DrainGangScholar <veljko.markovic@elfak.rs>
2023-10-11 21:00:13 +0200

    lol

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+use warp::http::StatusCode;
+use warp::reject::{Reject, Rejection};
 use warp::Filter;
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -9,7 +12,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -22,7 +25,6 @@ impl Question {
         }
     }
 }
-
 impl std::str::FromStr for QuestionId {
     type Err = std::io::Error;
     fn from_str(id: &str) -> Result<Self, Self::Err> {
@@ -32,20 +34,49 @@ impl std::str::FromStr for QuestionId {
         }
     }
 }
-#[tokio::main]
-async fn main() {
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+async fn qet_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("14").expect("No id provided"),
         String::from("title"),
         String::from("Hey, World?"),
-        Some(vec![String::from("nib")]),
+        Some(vec![
+            String::from("nib"),
+            String::from("bon"),
+            String::from("jin"),
+        ]),
     );
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
 
-    let hello=warp::get()
-        .map(move || format!("{:#?}",&question));
+async fn return_error(r: Rejection) -> Result<impl warp::Reply, Rejection> {
+    if let Some(_invalid_id) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
 
-    warp::serve(hello)
-        .run(([127,0,0,1],3030))
-        .await;
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(qet_questions)
+        .recover(return_error);
 
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
